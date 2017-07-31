d25af1f328f72f92dc9dac7d9438264d1375042c
jsmolka <jsmolka@pm.me>
2017-07-31 23:01:46 +0200

    choose algorithm to display

--- maze/benchmark/results.py
+++ maze/benchmark/results.py
@@ -12,7 +12,11 @@ draw.menu("Which results do you want to display?",
 
 method = dialog.user_input(1, 2)
 
+draw.line()
+
 if method == 1:  # Show create
+    method = "create"
+
     algorithm_list = [
         Algorithm.Create.BACKTRACKING.value,
         Algorithm.Create.HUNT.value,
@@ -21,35 +25,38 @@ if method == 1:  # Show create
         Algorithm.Create.PRIM.value,
         Algorithm.Create.KRUSKAL.value
     ]
-    method = "create"
+
 else:  # Show solve
+    method = "solve"
+
     algorithm_list = [
         Algorithm.Solve.BACKTRACKING.value
     ]
-    method = "solve"
 
-for algorithm in algorithm_list:
-    draw.heading(algorithm)
-    value_list = db.filter(algorithm, method)
+draw.menu("Which algorithm do you want to display?", algorithm_list)
+algorithm = algorithm_list[dialog.user_input([1, len(algorithm_list) + 1], create_range=True) - 1]
 
-    for value in value_list:
-        iteration = value[4]
-        if len(iteration) == 1:
-            iteration = "  {0}".format(iteration)
-        elif len(iteration) == 2:
-            iteration = " {0}".format(iteration)
-        system = value[3]
-        if system == "PC":
-            system = " PC"
+draw.heading(algorithm)
+value_list = db.filter(algorithm, method)
 
-        print("Version: {0} | system: {1} | iterations: {2} | size: {3} | average time: {4}".format(
-            value[2],
-            system,
-            iteration,
-            value[5],
-            value[6]
-        ))
+for value in value_list:
+    iteration = value[4]
+    if len(iteration) == 1:
+        iteration = "  {0}".format(iteration)
+    elif len(iteration) == 2:
+        iteration = " {0}".format(iteration)
+    system = value[3]
+    if system == "PC":
+        system = " PC"
 
-    print("\n")
+    print("Version: {0} | system: {1} | iterations: {2} | size: {3} | average time: {4}".format(
+        value[2],
+        system,
+        iteration,
+        value[5],
+        value[6]
+    ))
+
+draw.line()
 
 dialog.enter("exit")
