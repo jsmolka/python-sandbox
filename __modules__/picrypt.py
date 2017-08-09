b808945d329a7cc050babe36a520dcf1ff8d2078
jsmolka <jsmolka@pm.me>
2017-08-09 18:24:50 +0200

    add support for all characters

--- __modules__/picrypt.py
+++ __modules__/picrypt.py
@@ -3,9 +3,6 @@ from random import randint
 from math import sqrt, ceil
 from PIL import Image
 
-# Ideas
-# Uses [r, g, b] as separate bytes
-# [bin(r), bin(g), bin(b)] = 2^24 possible numbers -> Unicode maybe?
 
 class Picrypt:
     def __init__(self):
@@ -31,20 +28,22 @@ class Picrypt:
 
     def encrypt(self, s):
         """Encrypts a string"""
+
+        def int_to_rgb(v, o):
+            """Converts int to rgb"""
+            rgb = "{:024b}".format(v - o)
+            return [int(rgb[:8], 2), int(rgb[8:16], 2), int(rgb[16:], 2)]
+
         n = len(s)
         rpi = list(reversed(Picrypt.pi(n)))
         pxl = list()
-        print(n)
-        pxl.append(n)
+        pxl.append(int_to_rgb(n, 0))
         for i in range(0, n):
-            o = ord(s[i]) - rpi[-i - 1]
-            if not 0 <= o < 128:
-                raise Exception("Picrypt is only defined for ASCII characters")
-            pxl.append(o)
+            pxl.append(int_to_rgb(ord(s[i]), rpi[-i - 1]))
             for j in range(0, rpi[i]):
-                pxl.append(randint(0, 128))
+                pxl.append([0, 0, randint(0, 256)])
         sqr = int(ceil(sqrt(len(pxl))))
-        pic = np.zeros((sqr, sqr), dtype=np.uint8)
+        pic = np.zeros((sqr, sqr, 3), dtype=np.uint8)
         r = c = 0
         for i in range(0, sqr ** 2):
             if c == sqr:
@@ -53,24 +52,32 @@ class Picrypt:
             if i < len(pxl):
                 pic[r, c] = pxl[i]
             else:
-                pic[r, c] = randint(0, 128)
+                pic[r, c] = [0, 0, randint(0, 256)]
             c += 1
-        self.image = Image.fromarray(pic, "L")
+        self.image = Image.fromarray(pic, "RGB")
         return self.image
 
     def decrypt(self, img):
         """Decrypts an image"""
+
+        def rgb_to_int(v, o):
+            """Converts rgb to int"""
+            r = "{:08b}".format(v[0])
+            g = "{:08b}".format(v[1])
+            b = "{:08b}".format(v[2])
+            return int(r + g + b, 2) + o
+
         a = np.array(img, dtype=np.uint8)
         pxl = list()
         for i in range(0, len(a)):
             for j in range(0, len(a[0])):
                 pxl.append(a[i, j])
-        n = pxl.pop(0)
+        n = rgb_to_int(pxl.pop(0), 0)
         rpi = list(reversed(Picrypt.pi(n)))
         self.text = ""
         j = 0
         for i in range(0, n):
-            self.text += chr(pxl[j] + rpi[-i - 1])
+            self.text += chr(rgb_to_int(pxl[j], rpi[-i - 1]))
             j += rpi[i] + 1
         return self.text
 
@@ -98,18 +105,3 @@ class Picrypt:
         with open(file_name, "r") as f:
             s = f.read()
         return s
-
-
-pc = Picrypt()
-pc.encrypt(Picrypt.load_text("in.txt"))
-pc.save_image("test.png")
-pc.decrypt(Picrypt.load_image("test.png"))
-pc.save_text("out.txt")
-
-
-
-
-
-
-
-
