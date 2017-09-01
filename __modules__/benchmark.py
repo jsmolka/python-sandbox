2e681c9214b7527dbf807d763f61d0ca749be8bb
jsmolka <jsmolka@pm.me>
2017-09-01 22:05:37 +0200

    factor instead of percentage

--- __modules__/benchmark.py
+++ __modules__/benchmark.py
@@ -3,7 +3,7 @@ from time import clock
 
 def benchmark(f, a, n, calibrate=False):
     """Benchmarks a function"""
-    print("##### {0} #####".format(f.__name__))
+    print("########## {0} ##########".format(f.__name__))
     r = range(n)
     offset = 0
     if calibrate:  # Minimize function call overhead
@@ -17,9 +17,10 @@ def benchmark(f, a, n, calibrate=False):
     for i in r:  # Minimize loop overhead
         f(a); f(a); f(a); f(a); f(a); f(a); f(a); f(a); f(a); f(a)
     t2 = clock()
-    time = round(t2 - t1 - offset, 5)
+    raw_time = t2 - t1 - offset
+    time = round(raw_time, 5)
     print("total time:", time)
-    print("loop time:", round(time / (10 * n), 5))
+    print("loop time:", round(raw_time / (10 * n), 5))
     return time
 
 
@@ -29,8 +30,8 @@ def compare(f1, f2, a1, a2, n, calibrate=False):
     t2 = benchmark(f2, a2, n, calibrate=calibrate)
 
     if t1 / t2 < 1:
-        print("{0} is {2}% faster than {1}".format(f1.__name__, f2.__name__, round((1 - t1 / t2) * 100, 2)))
+        print("{0} is {2}x faster than {1}".format(f1.__name__, f2.__name__, round(1 / (t1 / t2), 2)))
     elif t2 / t1 < 1:
-        print("{0} is {2}% faster than {1}".format(f2.__name__, f1.__name__, round((1 - t2 / t1) * 100, 2)))
+        print("{0} is {2}x faster than {1}".format(f2.__name__, f1.__name__, round(1 / (t2 / t1), 2)))
     else:
         print("{0} and {1} perform exactly the same".format(f1.__name__, f2.__name__))
