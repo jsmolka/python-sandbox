556be722b66ba7b7e204450ab4a580b46f0cda64
jsmolka <jsmolka@pm.me>
2017-09-28 01:09:23 +0200

    update link script for atom settings

--- scripts/link.py
+++ scripts/link.py
@@ -1,13 +1,24 @@
-import admin
+import ctypes
 import os
+import sys
+from pathlib import Path
+
+
+def admin_check(file_name):
+    """Checks if program is running with admin privileges and restarts it if it is not"""
+    if not ctypes.windll.shell32.IsUserAnAdmin():  # Check if user is admin
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, file_name, None, 1)  # Restart as admin
+        sys.exit()  # Exit old instance
 
 
 def create_link(source, target=None):
     """Creates link"""
     if target is None:
         target = source
-    os.system("mklink /d \"C:\\Users\\Julian\\{0}\" \"C:\\Users\\Julian\\OneDrive\\{1}\"".format(target, source))
+    os.system("mklink /d \"{0}\\{1}\" \"{0}\\OneDrive\\{2}\"".format(str(Path.home()), target, source))
 
+
+admin_check(__file__)
 
 create_link("Coding")
 create_link("Studium")
@@ -15,3 +26,4 @@ create_link("Sonstiges")
 create_link("Documents\\Dolphin Emulator")
 create_link("Documents\\Outlook-Dateien")
 create_link("Pictures")
+create_link("Documents\\Settings\\Atom\\.atom", ".atom")
