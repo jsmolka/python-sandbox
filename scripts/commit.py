5d9d22d1280c4183b28838f768c3264aefcaeb23
jsmolka <jsmolka@pm.me>
2017-09-28 01:11:34 +0200

    remove script

--- scripts/commit.py
+++
@@ -1,8 +0,0 @@
-import os
-
-os.chdir("C:\\Users\\Julian\\OneDrive\\Coding\\Python")
-
-print("Input commit message!")
-message = input()
-
-os.system("git add --all & git commit -m \"{0}\" & git push -u origin master".format(message))
