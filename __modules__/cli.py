32fff742c7891e9057225aec4f78474aa9ec856c
jsmolka <jsmolka@pm.me>
2017-10-05 19:48:43 +0200

    use ctypes because color do not work

--- __modules__/cli.py
+++ __modules__/cli.py
@@ -1,12 +1,76 @@
-class Color:
-    RED     = "\033[01;31m"
-    GREEN   = "\033[01;32m"
-    YELLOW  = "\033[01;33m"
-    BLUE    = "\033[01;34m"
-    MAGENTA = "\033[01;35m"
-    CYAN    = "\033[01;36m"
-    WHITE   = "\033[01;37m"
-    DEFAULT = "\033[m"
+from ctypes import windll, Structure, c_short, c_ushort, byref
+
+SHORT = c_short
+WORD = c_ushort
+
+
+class COORD(Structure):
+    """struct in wincon.h"""
+    _fields_ = [
+        ("X", SHORT),
+        ("Y", SHORT)]
+
+
+class SMALL_RECT(Structure):
+    """struct in wincon.h"""
+    _fields_ = [
+        ("Left", SHORT),
+        ("Top", SHORT),
+        ("Right", SHORT),
+        ("Bottom", SHORT)]
+
+
+class CONSOLE_SCREEN_BUFFER_INFO(Structure):
+    """struct in wincon.h"""
+    _fields_ = [
+        ("dwSize", COORD),
+        ("dwCursorPosition", COORD),
+        ("wAttributes", WORD),
+        ("srWindow", SMALL_RECT),
+        ("dwMaximumWindowSize", COORD)]
+
+
+# winbase.h
+STD_INPUT_HANDLE  = -10
+STD_OUTPUT_HANDLE = -11
+STD_ERROR_HANDLE  = -12
+
+# wincon.h
+FG_BLACK     = 0x0000
+FG_BLUE      = 0x0001
+FG_GREEN     = 0x0002
+FG_CYAN      = 0x0003
+FG_RED       = 0x0004
+FG_MAGENTA   = 0x0005
+FG_YELLOW    = 0x0006
+FG_GREY      = 0x0007
+FG_INTENSITY = 0x0008
+
+BG_BLACK     = 0x0000
+BG_BLUE      = 0x0010
+BG_GREEN     = 0x0020
+BG_CYAN      = 0x0030
+BG_RED       = 0x0040
+BG_MAGENTA   = 0x0050
+BG_YELLOW    = 0x0060
+BG_GREY      = 0x0070
+BG_INTENSITY = 0x0080
+
+stdout_handle = windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
+SetConsoleTextAttribute = windll.kernel32.SetConsoleTextAttribute
+GetConsoleScreenBufferInfo = windll.kernel32.GetConsoleScreenBufferInfo
+
+
+def get_text_attr():
+    """Returns the character attributes of the console screen buffer"""
+    csbi = CONSOLE_SCREEN_BUFFER_INFO()
+    GetConsoleScreenBufferInfo(stdout_handle, byref(csbi))
+    return csbi.wAttributes
+
+
+def set_text_attr(color):
+  """Sets the character attributes of the console screen buffer"""
+  SetConsoleTextAttribute(stdout_handle, color)
 
 
 def c_print(text):
@@ -14,14 +78,4 @@ def c_print(text):
     This function prints text in different colors.
     Use <color>text<default> for a different text color.
     """
-    text = text \
-        .replace("<red>", Color.RED) \
-        .replace("<green>", Color.GREEN) \
-        .replace("<yellow>", Color.YELLOW) \
-        .replace("<blue>", Color.BLUE) \
-        .replace("<magenta>", Color.MAGENTA) \
-        .replace("<cyan>", Color.CYAN) \
-        .replace("<white>", Color.WHITE) \
-        .replace("<default>", Color.DEFAULT)
-
-    print(text + Color.DEFAULT)
+    pass
