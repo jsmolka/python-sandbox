67791c9a9069a8eb527a99b821d84c39f0c2a49e
jsmolka <jsmolka@pm.me>
2017-10-06 09:24:43 +0200

    add functions

--- __modules__/cli.py
+++ __modules__/cli.py
@@ -1,6 +1,9 @@
 from ctypes import windll, Structure, c_short, c_ushort, byref
+from math import ceil, floor
+from shutil import get_terminal_size
 
-__all__ = ["Color", "c_print"]
+__all__ = ["Color", "c_print", "LineStyle", "line", "heading", "big_heading",
+           "menu", "progress_bar", "yes_no"]
 
 SHORT = c_short
 WORD = c_ushort
@@ -70,20 +73,127 @@ class Color:
     DEFAULT    = get_color()
 
 
-def c_print(string, *colors):
+def c_print(string, *colors, end=""):
     """Prints string in a different color"""
     if not colors:
-        return print(string)
+        return print(string, end=end)
 
     color = 0x0000
     for c in colors:
         color |= c
     set_color(color)
-    print(string)
+    print(string, end=end)
     set_color(Color.DEFAULT)
 
 
-class LineType:
+class LineStyle:
     SCORE      = "-"
     UNDERSCORE = "_"
     HASH       = "#"
+
+
+def terminal_size():
+    """Returns terminal size"""
+    return get_terminal_size()[0] - 1
+
+
+def line(style=LineStyle.SCORE, color=()):
+    """Draws line"""
+    l = style * terminal_size()
+    c_print(l, *color)
+
+
+def heading(caption, style=LineStyle.HASH, color=()):
+    """Draws heading"""
+    size = terminal_size() - len(caption) - 2
+    caption = "{0} {1} {2}".format(
+        ceil(size / 2) * style,
+        caption,
+        floor(size / 2) * style
+    )
+    c_print(caption, *color)
+
+
+def big_heading(caption, style=LineStyle.HASH, color=()):
+    """Draws big heading"""
+    line(style=style, color=color)
+    heading(caption, style=style, color=color)
+    line(style=style, color=color)
+
+
+def menu(caption, *entries, caption_color=(), entry_color=()):
+    """Draws menu"""
+    if not isinstance(caption_color, tuple):
+        caption_color = [caption_color]
+    if not isinstance(entry_color, tuple):
+        entry_color = [entry_color]
+
+    c_print(caption, *caption_color)
+    i = 0
+    for entry in entries:
+        i += 1
+        c_print("[{0}] ".format(i) + entry, *entry_color)
+
+
+def progress_bar(iteration, total, prefix="Progress:", suffix="", decimals=1, length=25, fill="█", color=()):
+    """Draws progress bar"""
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filled_length = length * iteration // total
+    bar = fill * filled_length + "-" * (length - filled_length - 1)
+    c_print("\r%s |%s| %s%% %s" % (prefix, bar, percent, suffix), *color, end="\r")
+    if iteration == total:
+        print()
+
+
+def yes_no(message, message_color=(), error_color=()):
+    """Prints yes/no dialog"""
+    c_print(message + " (y/n)", *message_color)
+    while True:
+        answer = input()
+        if answer == "y":
+            return True
+        elif answer == "n":
+            return False
+        else:
+            c_print("Invalid answer!", *error_color)
+
+
+def enter(action):
+    """Prints enter message"""
+    input("Press enter to {0}...".format(action))
+
+
+def user_input(*answers, range_=False):
+    """Processes user input"""
+    if not answers:
+        raise Exception("Answers cannot be empty")
+
+    if len(answers) == 1 and type(answers[0]) == list:  # Extract list
+        answers = answers[0]
+
+    if range_ and len(answers) != 2:
+        raise Exception("Create range is only defines for two values in the answer list")
+
+    answers = list(answers)
+    if range_:
+        if type(answers[0]) == int and type(answers[1]) == int:
+            answers.sort()
+            start = answers[0]
+            end = answers[1] + 1
+            answers = []
+            for i in range(start, end):
+                answers.append(i)
+
+    answer_type = type(answers[0])
+
+    while True:
+        answer = input()
+
+        try:
+            answer = answer_type(answer)
+            if answer in answers:
+                return answer
+            else:
+                print("Invalid answer! Try again!")
+        except:
+            print("Invalid answer! Try again!")
