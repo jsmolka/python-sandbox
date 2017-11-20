b70a68a48c63e33dea9890f1618aa21af7a0a21e
jsmolka <jsmolka@pm.me>
2017-11-20 09:07:46 +0100

    change for updated cli

--- test/cli/draw.py
+++ test/cli/draw.py
@@ -1,15 +1,15 @@
 from cli import *
 
-c_print("a", "b", "c", color=[Color.CYAN, Color.INTENSE])
-line(LineStyle.HASH, color=[Color.CYAN, Color.INTENSE])
-line(LineStyle.SCORE, color=[Color.CYAN, Color.INTENSE])
-line(LineStyle.UNDERSCORE, color=[Color.CYAN, Color.INTENSE])
-heading("test", style=LineStyle.HASH ,color=[Color.CYAN, Color.INTENSE])
-heading("test", style=LineStyle.SCORE ,color=[Color.CYAN, Color.INTENSE])
-heading("test", style=LineStyle.UNDERSCORE ,color=[Color.CYAN, Color.INTENSE])
-big_heading("test", style=LineStyle.HASH ,color=[Color.CYAN, Color.INTENSE])
-big_heading("test", style=LineStyle.SCORE ,color=[Color.CYAN, Color.INTENSE])
-big_heading("test", style=LineStyle.UNDERSCORE ,color=[Color.CYAN, Color.INTENSE])
+cprint("a", "b", "c", color=[Color.CYAN, Color.INTENSE])
+line(Style.HASH, color=[Color.CYAN, Color.INTENSE])
+line(Style.SCORE, color=[Color.CYAN, Color.INTENSE])
+line(Style.UNDERSCORE, color=[Color.CYAN, Color.INTENSE])
+heading("test", style=Style.HASH ,color=[Color.CYAN, Color.INTENSE])
+heading("test", style=Style.SCORE ,color=[Color.CYAN, Color.INTENSE])
+heading("test", style=Style.UNDERSCORE ,color=[Color.CYAN, Color.INTENSE])
+big_heading("test", style=Style.HASH ,color=[Color.CYAN, Color.INTENSE])
+big_heading("test", style=Style.SCORE ,color=[Color.CYAN, Color.INTENSE])
+big_heading("test", style=Style.UNDERSCORE ,color=[Color.CYAN, Color.INTENSE])
 menu("caption", "entry", "entry", "entry", color=[Color.CYAN, Color.INTENSE])
 n = 10000
 for i in range(0, n):
