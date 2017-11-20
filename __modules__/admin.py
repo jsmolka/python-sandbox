d8aed385649828b4a086a1714ada449493660458
jsmolka <jsmolka@pm.me>
2017-11-20 08:31:50 +0100

    remove auto execution

--- __modules__/admin.py
+++ __modules__/admin.py
@@ -1,4 +1,3 @@
-import __main__
 import ctypes
 import sys
 
@@ -8,7 +7,3 @@ def check(file_name):
     if not ctypes.windll.shell32.IsUserAnAdmin():  # Check if user is admin
         ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, file_name, None, 1)  # Restart as admin
         sys.exit()  # Exit old instance
-
-
-if __name__ != "__main__":  # Run if admin module is not main module
-    check(__main__.__file__)  # Automatically check main module at import
