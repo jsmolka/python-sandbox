efb16b412cc9ba3861b0f11fd19190ff5d28db08
jsmolka <jsmolka@pm.me>
2018-02-12 17:19:11 +0100

    add utility functions

--- __modules__/utils.py
+++ __modules__/utils.py
@@ -1,45 +1,154 @@
-from random import shuffle
+def remap(val, l1, h1, l2, h2):
+    """
+    Re-maps a value from one range to another.
+    
+    :param val: values to be remapped
+    :param l1: minimum value of first range
+    :param h1: maximum value of first range
+    :param l2: minimum value of second range
+    :param h2: maximum value of second range
+    :returns: remapped value
+    """
+    return float(val - l1) / (h1 - l1) * (h2 - l2) + l2
 
 
-def create_class(name, keys, values=None):
-    """Creates object dynamically"""
-    cls = type(name, (object,), {})
-    for idx, key in enumerate(keys):
-		value = values[idx] if values else None
-        setattr(cls, key, value)
-    return cls
-
-
-def my_dict(obj):
-    """Converts object into dictionary"""
-    if not hasattr(obj, "__dict__"):
-        return obj
-    result = {}
-    for key, value in obj.__dict__.items():
-        if key.startswith("_"):
+def chunk(lst, n):
+    """
+    Splits a list into n equally sized chunks.
+    
+    :param lst: list to process
+    :param n: amount of chunks
+    :returns: list of chunks
+    """
+    return [lst[i:i + n] for i in range(0, len(lst), n)]
+    
+    
+def _unique(lst, key):
+    """
+    Removes duplicates from a list.
+    
+    :param lst: list to process
+    :param key: key for removing duplicates
+    :returns: generator without duplicates
+    """
+    seen = set()
+    for x in lst:
+        val = key(x)
+        if val in seen:
             continue
-        element = []
-        if isinstance(value, list):
-            for item in value:
-                element.append(my_dict(item))
-        else:
-            element = my_dict(value)
-        result[key] = element
-    return result
+        seen.add(val)
+        yield x
+    
+    
+def unique(lst, key=lambda x: x):
+    """
+    Removes duplicates from a list.
+    
+    :param lst: list to process
+    :param key: key for removing duplicates
+    :returns: list without duplicates
+    """
+    return list(_unique(lst, key))
+    
+    
+def compact(lst):
+    """
+    Removes all falsey values.
+    
+    :param lst: list to process
+    :returns: list without false values
+    """
+    return [x for x in lst if x]
+    
+    
+def index(lst, val):
+    """
+    Finds first index of value in a list.
+    
+    :param lst: list to process
+    :param val: value to be searched
+    :returns: first index of value
+    """
+    try:
+        return lst.index(val)
+    except ValueError:
+        return -1
+        
+        
+def indices(lst, val):
+    """
+    Finds all indices of value in a list.
+    
+    :param lst: list to process
+    :param val: value to be searched
+    :returns: list of indices
+    """
+    return [idx for idx, x in enumerate(lst) if x == val]
+    
+    
+def _flatten(lst):
+    """
+    Flattens list.
+    
+    :param lst: list to be flattened
+    :returns: generator of flattened list
+    """
+    for x in lst:
+        try:
+            yield from _flatten(x)
+        except TypeError:
+            yield x
 
-
-def remap(v, l1, h1, l2, h2):
-    """Re-maps a number from one range to another"""
-    return float(v - l1) / (h1 - l1) * (h2 - l2) + l2
-
-
-def shuffled(lst):
-    """Returns shuffled list"""
-    result = lst[:]
-    shuffle(result)
-    return result
-
-	
-def chunk_list(lst, count):
-	"""Split list in equally sizes chunks"""
-	return [lst[i:i + count] for i in range(0, len(lst), count)]
+    
+def flatten(lst):
+    """
+    Flattens list.
+    
+    :param lst: list to be flattened
+    :returns: flattened list
+    """
+    return list(_flatten(lst))
+    
+    
+def union(lst, *others):
+    """
+    Creates union of passed lists.
+    
+    :param lst: list to union with
+    :param others: lists to unionize with
+    :returns: unionized list
+    """
+    return unique(flatten(lst + list(others)))
+    
+    
+def without(lst, *vals):
+    """
+    Creates list without values.
+    
+    :param lst: list to process
+    :param vals: values to be removed
+    :returns: list without values
+    """
+    return [x for x in lst if x not in vals]
+    
+    
+def where(lst, key):
+    """
+    Finds items which evaluate key to true.
+    
+    :param lst: list to process
+    :param key: key to evaluate
+    :returns: list of items which evaluate key to true
+    """    
+    return [x for x in lst if key(x)]
+    
+    
+def times(n, func):
+    """
+    Returns list with n-times the function result.
+    
+    :param n: number of repeats
+    :param func: function called for each iteration
+    :returns: list of n-times the function result
+    """
+    return [func() for i in range(n)]
