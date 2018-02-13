73204617e0b56e6c10b43d44ba35d1db2a45ff5a
jsmolka <jsmolka@pm.me>
2018-02-13 18:57:30 +0100

    add functions and change docstrings

--- __modules__/utils.py
+++ __modules__/utils.py
@@ -1,17 +1,28 @@
-def remap(val, l1, h1, l2, h2):
+def remap(value, min1, max1, min2, max2):
     """
     Re-maps a value from one range to another.
     
-    :param val: values to be remapped
-    :param l1: minimum value of first range
-    :param h1: maximum value of first range
-    :param l2: minimum value of second range
-    :param h2: maximum value of second range
+    :param value: values to be remapped
+    :param min1: minimum value of first range
+    :param max1: maximum value of first range
+    :param min2: minimum value of second range
+    :param max2: maximum value of second range
     :returns: remapped value
     """
-    return float(val - l1) / (h1 - l1) * (h2 - l2) + l2
+    return (value - min1) / (max1 - min1) * (max2 - min2) + min2
 
-
+	
+def sized(lst, n):
+	"""
+    Splits a list into n-sized chunks.
+    
+    :param lst: list to process
+    :param n: size of chunks
+    :returns: list of chunks
+    """
+	return [lst[i:i + n] for i in range(0, len(lst), n)]
+    
+    
 def chunk(lst, n):
     """
     Splits a list into n equally sized chunks.
@@ -20,7 +31,9 @@ def chunk(lst, n):
     :param n: amount of chunks
     :returns: list of chunks
     """
-    return [lst[i:i + n] for i in range(0, len(lst), n)]
+    quo, rem = divmod(len(lst), n)
+    idc = [quo * i + min(i, rem) for i in range(n + 1)]
+    return [lst[idc[i]:idc[i + 1]] for i in range(n)]
     
     
 def _unique(lst, key):
@@ -46,7 +59,7 @@ def unique(lst, key=lambda x: x):
     
     :param lst: list to process
     :param key: key for removing duplicates
-    :returns: list without duplicates
+    :returs: list without duplicates
     """
     return list(_unique(lst, key))
     
@@ -56,34 +69,34 @@ def compact(lst):
     Removes all falsey values.
     
     :param lst: list to process
-    :returns: list without false values
+    :returns: list without falsey values
     """
     return [x for x in lst if x]
     
     
-def index(lst, val):
+def index(lst, value):
     """
     Finds first index of value in a list.
     
     :param lst: list to process
-    :param val: value to be searched
+    :param value: value to be searched
     :returns: first index of value
     """
     try:
-        return lst.index(val)
+        return lst.index(value)
     except ValueError:
         return -1
         
         
-def indices(lst, val):
+def indices(lst, value):
     """
     Finds all indices of value in a list.
     
     :param lst: list to process
-    :param val: value to be searched
+    :param value: value to be searched
     :returns: list of indices
     """
-    return [idx for idx, x in enumerate(lst) if x == val]
+    return [idx for idx, x in enumerate(lst) if x == value]
     
     
 def _flatten(lst):
@@ -121,20 +134,34 @@ def union(lst, *others):
     return unique(flatten(lst + list(others)))
     
     
-def without(lst, *vals):
+def without(lst, *values):
     """
     Creates list without values.
     
     :param lst: list to process
-    :param vals: values to be removed
+    :param values: values to be removed
     :returns: list without values
     """
-    return [x for x in lst if x not in vals]
+    values = set(values)
+    return [x for x in lst if x not in values]
     
+    
+def first(lst, key):
+    """
+    Finds first item which evaluates key to true.
+    
+    :param lst: list to process
+    :param key: key to evaluate
+    :returns: first item which evaluates key to true.
+    """
+    for x in lst:
+        if key(x):
+            return x
+
     
 def where(lst, key):
     """
-    Finds items which evaluate key to true.
+    Finds all items which evaluate key to true.
     
     :param lst: list to process
     :param key: key to evaluate
