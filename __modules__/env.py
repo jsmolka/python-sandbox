5e4e912879df1fca71bf171a1fcd26f01293a2f8
jsmolka <jsmolka@pm.me>
2018-03-03 17:29:28 +0100

    allow multiple environ changes

--- __modules__/env.py
+++ __modules__/env.py
@@ -2,6 +2,42 @@ import os
 import shared
 
 
+_changes = {}
+
+
+def current(env):
+    """
+    Read the current value of an environment variable.
+
+    :param env: environment variable
+    :return: str
+    """
+    pths = ""
+    if env in os.environ:
+        pths = os.environ[env].rstrip(";")
+    if env in _changes:
+        if pths:
+            pths += ";"
+        pths += ";".join(_changes[env])
+    return pths
+
+
+def _push_change(env, pth):
+    """
+    Pushes an change.
+
+    :param env: environment variable
+    :param pth: path to add
+    :return: None
+    """
+    if env not in _changes:
+        _changes[env] = []
+    if not pth:
+        raise Exception("Path to add is invalid: <{}>".format(pth))
+    if pth not in _changes[env]:
+        _changes[env].append(pth)
+
+
 def add(env, pth, stdout=True, stderr=True):
     """
     Adds path to system environment.
@@ -12,8 +48,6 @@ def add(env, pth, stdout=True, stderr=True):
     :param stderr: show stderr
     :returns: int
     """
-    if env in os.environ:
-        if pth not in os.environ[env].split(";"):
-            return shared.execute("setx {0} \"%{0}%;{1}\"".format(env, pth), stdout, stderr)
-    else:
-        return shared.execute("setx {} \"{}\"".format(env, pth), stdout, stderr)
+    cmd = "powershell [Environment]::SetEnvironmentVariable('{}', '{}', 'User')"
+    _push_change(env, pth)
+    return shared.execute(cmd.format(env, current(env)), stdout, stderr)
